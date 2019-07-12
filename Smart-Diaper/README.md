216407c5433dcc0c6f9e36499cbdc3da9ea4899e
Rohan <34987773+RohanShekhawat@users.noreply.github.com>
2019-07-12 14:38:48 +0530

    Add files via upload

---
+++ Smart-Diaper/README.md
@@ -0,0 +1,46 @@
+# Smart-Baby-Diaper
+
+### INTRODUCTION AND OVERVIEW
+Used Octabrix Dev kit for Smart baby diaper. 
+Diaper detects the moisture of the diaper and at appropriate levels
+of moisture sends notification to Blynk app as well as 
+sends an email as well as plots a real time graph for the moisture level in the app
+
+### COMPONENTS
+#### Hardware :
+- Octabrix
+- FC-28 humidity sensor
+- Jumper Wires
+- Paper cups(for making temporary case)
+- 9v DC power supply
+- Android phone with Blynk App
+- Baby Diaper
+#### Software :
+- Arduino IDE
+#### Cloud platforms :
+- Blynk
+- IFTTT
+
+### WORKING 
+As soon as the moisture level starts increasing in the diaper
+the humidity sensor FC-28 starts sending data to octabrix ,now octabrix 
+these reading to blynk cloud and also plots the real time graph of the 
+recieved data. At different level of moisture a notification is recieved 
+in the app and well as an email notification is recieved.
+
+### FUTURE MODIFICATIONS
+1. ##### Sound Detection
+  we can install sound sensors so that when baby cries it will notify the parent
+  
+2. ##### Tracking System
+  we can also install GPS Tracking system so that parent can easily track their children when
+  lost.
+
+![screenshot_2018-07-13-14-35-56-480_cc blynk](https://user-images.githubusercontent.com/41651033/43277905-1dd2c0f2-9127-11e8-8481-4ac7a4aa7b24.png)
+
+
+![screenshot_2018-07-13-14-52-26-532_lockscreen](https://user-images.githubusercontent.com/41651033/43277983-57e45558-9127-11e8-97fa-c15ff70c0af8.png)
+
+
+![img-20180726-wa0022](https://user-images.githubusercontent.com/41651033/43278039-7f563b6a-9127-11e8-873c-2a3a6c8e163b.jpg)
+
---
+++ Smart-Diaper/Smart_Diaper.ino
@@ -0,0 +1,60 @@
+#define BLYNK_PRINT Serial                          //  For Serial printing the connection status and IP Adress
+#include<BlynkSimpleEsp8266.h>                      //  Adding Library for Blynk and WiFi
+#include<ESP8266WiFi.h> 
+
+int x=0;
+int y;                                              //  Defining Variable for iterator and to store value
+int i=0;
+char auth[]="ae9da73e507240ad83395238c8d41fe5";     //  Authentivcation code for Blynk app
+char ssid[]="iBHubs";       
+char pass[]="ibhubsguest";                          //  Storing the network name and password in variable
+BlynkTimer timer;                                   //  Creating object timer of class BlynkTimer
+void setup() {
+  // put your setup code here, to run once:
+    Serial.begin(115200);                           //  Starting the serial monitor and setting up the baud rate
+    Blynk.begin(auth,ssid,pass);                    //  Here Octabrix connects to blynk cloud 
+    pinMode(A0,INPUT);                              //  Defining the pin for recieving data
+    timer.setInterval(1000L,myTimerEvent);          //  setting time interval of sending consecutive data
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Blynk.run() ;                                     //  to process incoming request and do the housekeeping of blynk connected
+  timer.run();
+   
+     
+}
+void myTimerEvent()                                 //  Function for sending data and Triggering Notifications
+{
+  i++;
+  y=analogRead(A0);                                 //  Recieving the data from sensor
+  Serial.print(" Y IS : ");
+  Serial.println(y);                                //  printing data on serial monitor
+  x=x+y;
+  if(i==5)                                          //  Taking average of 5 reading consecutive raeding
+  {
+    x=x/5;
+  Blynk.virtualWrite(V5,(980-x));                   //  Sending the data to blynk cloud
+  Serial.print("average is : ");
+  Serial.println(x);
+  if(x>560&&x<650)                                  // Sending Email notification at certain values of humidity
+  {
+    Blynk.notify("Diaper is a little wet");
+    Blynk.email("iec2017054@iiita.ac.in","Baby Update","Diaper is a little wet");
+   }
+ if(x>450&&x<540)
+  {
+    Blynk.notify("Baby is feeling uncomfortable,its time to change");
+    Blynk.email("iec2017054@iiita.ac.in","Baby Update","Baby is feeling uncomfortable,its time to change");
+  }
+ if(x<370)
+ {
+  Blynk.notify("Hurry CHANGE THE DIAPER!!! Baby might get Rashes");
+  Blynk.email("iec2017054@iiita.ac.in","Baby Update","Hurry CHANGE THE DIAPER!!! Baby might get Rashes");
+  }
+  i=0;
+  x=0;
+  }
+ 
+}
+
